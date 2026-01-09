345dec641201eec12a271ea69cec734f94a41a3e
shafayet98 <shafayet.islam.61@gmail.com>
2026-01-09 22:24:30 +1030

    initialising the backend and health api check

---
+++ src/CatamacInvoice.API/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CatamacInvoice.API.Controllers
+{
+
+
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class HealthController: ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get() => Ok(new { status = "ok" });
+    }
+}
