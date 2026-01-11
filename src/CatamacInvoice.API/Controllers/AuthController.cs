fb70f1b461ed5be049ca30740556a9691e500826
shafayet98 <shafayet.islam.61@gmail.com>
2026-01-12 05:18:59 +1030

    added initial auth controller: register login

---
+++ src/CatamacInvoice.API/Controllers/AuthController.cs
@@ -0,0 +1,52 @@
+﻿using Catamac.Application.Dtos.Auth;
+using Catamac.Application.Services;
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CatamacInvoice.API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController: ControllerBase
+    {
+        public readonly AuthService _authService;
+
+        public AuthController(AuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<UserDto>> Register([FromBody] AuthRegisterRequest req)
+        {
+            try
+            {
+                var user = await _authService.RegisterAsync(req);
+                return Ok(user);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login([FromBody] AuthLoginRequest req)
+        {
+            try
+            { 
+                var result = await _authService.LoginAsync(req);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+
+        }
+
+    }
+}
